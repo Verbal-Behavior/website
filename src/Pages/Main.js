6e46a3420905fd1ffb3eaf83de018658e539e7bf
MrsWaifu <itsallgood4josh@yahoo.com>
2022-10-15 19:12:18 -0500

    Main menu started

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -3,40 +3,43 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Registration from './Registration';
 import Profile from './Profile';
-import './style.css';
-import image from './images/stars.png';
-import image2 from './images/moon.png';
-import image3 from './images/mountains_front.png';
-import image4 from './images/mountains_behind.png';
+import './Main.css';
 
 function Main() {
     return(
 <body>
         <header>
-            <a href="#" class="logo">logo</a>
-            <div>
-      <main>
-        <Routes>
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Registration' element={<Registration />} />
-          <Route path='/Profile' element={<Profile />} />
-          <Route path='/Main' element={<Main />} />
-        </Routes>
-      </main>
-    </div>
+            <a href="#" class="ProfileIcon">ProfileIcon</a>
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
         </header>
+        
     <section>
-        <img src={image} id="stars"/>
-        <img src={image2} id="moon"/>
-        <img src={image4} id="mountains_behind"/>
-        <h2 id="text">Moon Light</h2>
-        <a href="#sec" id="btn">Explore</a>
-        <img src={image3} id="mountains_front"></img>
-    </section>
-        <div class="sec" id="sec">
-            <h2>Effect</h2>
-            <p>WelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcomeWelcome</p>
-        </div>       
+        <h2 data-text="Create">Create</h2>
+        <div class="planets">
+            <div id="venus"></div>
+            <div id="star1"></div>
+           <div id="star2"></div>
+           <div id="star3"></div>
+           <div id="star4"></div>
+           <div id="star5"></div>
+           <div id="star6"></div>
+           <div id="star7"></div>
+           <div id="star8"></div>
+           <div id="star9"></div>
+        </div>
+        <div id="sun"></div>
+        <div class="meteor-cont">
+           <div class="meteor-ani">
+           <div id="meteor"></div>
+        </div>
+       </div>
+       <div class="stars"></div>
+    </section>     
 </body>  
     );
 }
