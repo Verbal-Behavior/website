6fa18e198e0608e10d3dd58d8d135c45be2cf2ca
Tanner Neuhaeusler <tneuhaeusler@gmail.com>
2022-10-30 08:55:59 -0400

    cards display from folder click

--- src/Pages/Cards.js
+++ src/Pages/Cards.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import CardsCSS from './Cards.module.css'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import {auth} from "../Firebase";
+import {db, auth} from "../Firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function Cards() {
+	const [flashcards, setFlashcards] = useState([]);
 	const [user, loading, error] = useAuthState(auth);
+	const flashcardsCollectionRef = collection(db, "flashcards");
 	const navigate = useNavigate();
 	const name = window.folderName;
+
+	const getFlashcards = async () => {
+		const q = query(flashcardsCollectionRef, where("uid", "==", user?.uid), where("folderName", "==", name))
+		const data = await getDocs(q);
+		setFlashcards(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+	}
+
+	getFlashcards();
   
 	useEffect(() => {
 		if (loading) return;
 		if (!user) return navigate("/");
   
 	}, [user, loading])
+
     return (
-<body>
-	<div className= {CardsCSS.card}>
-		<div className= {CardsCSS.card__inner}>
-               <div className= {CardsCSS.card__face__front}>
-				<h2 className= {CardsCSS.h2}>Test Text</h2>
-			</div>
-			<div className= {CardsCSS.card__face__back}>
-            <h2 className= {CardsCSS.h2}>{name}</h2>
-			</div>
-		</div>
-	</div>
-	<script src="main.js"></script>
-</body>
+	    <body>
+		{flashcards.map((flashcard)=> { 
+			return (
+				<div className= {CardsCSS.card}>
+				  <div className= {CardsCSS.card__inner}>
+					<div className= {CardsCSS.card__face__front}>
+					  <h2 className= {CardsCSS.h2}>{flashcard.frontText}</h2>
+					</div>
+					<div className= {CardsCSS.card__face__back}>
+					  <h2 className= {CardsCSS.h2}>{flashcard.backText}</h2>
+					</div>
+				  </div>
+				</div>
+			);
+		})}
+		<script src="main.js"></script>
+		</body>
     );
   }
   
